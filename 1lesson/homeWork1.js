16292d5a24759f34d8168c393f15d6ccc36ea907
Lidiia <l.potryasaeva@gmail.com>
2022-02-15 19:51:49 +0300

    1lesson

---
+++ 1lesson/homeWork1.js
@@ -0,0 +1,5 @@
+'use strict';
+const celsiusTemperature = prompt('enter temperature in Celsius');
+const fahrenheitTemperature = (9 / 5) * celsiusTemperature + 32;
+document.write('temperature in Fahrenheit: ' + fahrenheitTemperature)
+/*alert(fahrenheitTemperature);*/
